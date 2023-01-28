5aa7b5618a35369ad066b2e8df30c6e479d4a406
Andrey Gladyshev <m.agl7r@gmail.com>
2023-01-28 22:52:51 +0400

    build: вынести базовый URL API в переменные окружения

--- src/spa/api/utilityBills.js
+++ src/spa/api/utilityBills.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:8090/api/v1'
+const baseUrl = import.meta.env.API_BASE_URL
 
 export async function fetchUtilityBillsByYears(year) {
   const json = await fetch(`${baseUrl}/utility-bills/${year}`).then(response => response.json())
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
@@ -12,5 +11,6 @@ export default defineConfig({
     quasar({
       sassVariables: 'src/spa/css/quasar-variables.sass'
     })
-  ]
+  ],
+  envPrefix: 'API_'
 })
